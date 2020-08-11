a26626eced8a6451463fb8f2e7dd562754a7f495
Pierre-Luc Drouin <Pierre-Luc.Drouin@drdc-rddc.gc.ca>
2020-08-11 13:33:10 -0400

    -Removing dependency files from doxygen

--- html/files_dup.js
+++ html/files_dup.js
@@ -1,21 +1,16 @@
 var files_dup =
 [
     [ "args.c", "args_8c.html", "args_8c" ],
-    [ "args.d", "args_8d_source.html", null ],
     [ "args.h", "args_8h.html", "args_8h" ],
     [ "config.c", "config_8c.html", "config_8c" ],
-    [ "config.d", "config_8d_source.html", null ],
     [ "config.h", "config_8h.html", "config_8h" ],
     [ "main.c", "main_8c.html", "main_8c" ],
-    [ "main.d", "main_8d_source.html", null ],
     [ "main.h", "main_8h.html", "main_8h" ],
     [ "infindividual.h", "infindividual_8h.html", [
       [ "infindividual", "structinfindividual.html", "structinfindividual" ]
     ] ],
     [ "simulation.c", "simulation_8c.html", "simulation_8c" ],
-    [ "simulation.d", "simulation_8d_source.html", null ],
     [ "simulation.h", "simulation_8h.html", "simulation_8h" ],
     [ "standard_summary_stats.c", "standard__summary__stats_8c.html", "standard__summary__stats_8c" ],
-    [ "standard_summary_stats.d", "standard__summary__stats_8d_source.html", null ],
     [ "standard_summary_stats.h", "standard__summary__stats_8h.html", "standard__summary__stats_8h" ]
 ];
