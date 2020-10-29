64a86823e75b6108ccb63e2babd3f1e1aae36b2a
Pierre-Luc Drouin <Pierre-Luc.Drouin@drdc-rddc.gc.ca>
2020-10-29 10:14:44 -0400

    -Adding comment in ctout_read.py

--- python/ctout_read.py
+++ python/ctout_read.py
@@ -2,6 +2,7 @@
 Read a ctout output file from randoutbreaksim.
 
 Load timeline information from a ctout output file.
+Entry format is time of positive test time (in minutes), pre-symptomatic period duration(in minutes), child's ID, parent ID (negative if child's infectious period is not interrupted through CT), and the number of traced contacts.
 
 """
 
